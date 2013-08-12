726288ef29e6984d5d5106a46150bb8897538441
James Badger <jamesbadger@gmail.com>
2013-08-12 17:06:12 -0600

    Remove references to plugins
    No plugins in Rails 4

--- lib/mixable_engines.rb
+++ lib/mixable_engines.rb
@@ -4,7 +4,7 @@ module ActiveSupport::Dependencies
   def require_or_load(file_name, const_path = nil)
     if file_name.starts_with?(Rails.root.to_s + '/app')
       relative_name = file_name.gsub(Rails.root.to_s, '')
-      @engine_paths ||= plugin_and_engine_paths
+      @engine_paths ||= engine_paths
       @engine_paths.each do |path|
         engine_file = File.join(path, relative_name)
         require_or_load_without_multiple(engine_file, const_path) if File.file?(engine_file)
@@ -12,10 +12,10 @@ module ActiveSupport::Dependencies
     end
     require_or_load_without_multiple(file_name, const_path)
   end
-  
+
 private
-  def plugin_and_engine_paths
-    engines = Rails.application.class.railties.plugins + Rails.application.class.railties.engines
+  def engine_paths
+    engines = Rails.application.class.railties.engines
     engines.collect{|engine| engine.config.root.to_s }
   end
 end
